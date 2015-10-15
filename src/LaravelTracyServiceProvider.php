307977c2f17c34529866f0becee39d5189e9bb8e
unknown <recca0120@gmail.com>
2015-10-16 00:40:42 +0800

    ed

--- src/LaravelTracyServiceProvider.php
+++ src/LaravelTracyServiceProvider.php
@@ -50,18 +50,19 @@ class LaravelTracyServiceProvider extends ServiceProvider
                     'live' => true,
                 ],
             ], config('tracy'));
-
             Debugger::$time = array_get($_SERVER, 'REQUEST_TIME_FLOAT', microtime(true));
             Debugger::$maxDepth = array_get($config, 'maxDepth');
             Debugger::$maxLen = array_get($config, 'maxLen');
             Debugger::$showLocation = array_get($config, 'showLocation');
             Debugger::$strictMode = array_get($config, 'strictMode');
             Debugger::$editor = array_get($config, 'editor');
-
             foreach ($config['panels'] as $panel) {
                 Debugger::getBar()->addPanel(new $panel($config), $panel);
             }
-
+            $this->app->singleton(
+                'Illuminate\Contracts\Debug\ExceptionHandler',
+                'Recca0120\LaravelTracy\Exceptions\Handler'
+            );
             $kernel = $this->app['Illuminate\Contracts\Http\Kernel'];
             $kernel->pushMiddleware('Recca0120\LaravelTracy\Middleware\TracyMiddleware');
         }
