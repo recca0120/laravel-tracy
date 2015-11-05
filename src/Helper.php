17e98e4e01c6e736a6ecf95f645cfe36ab587ba6
unknown <recca0120@gmail.com>
2015-11-06 06:17:17 +0800

    if response is redirection don't do anything

--- src/Helper.php
+++ src/Helper.php
@@ -69,6 +69,9 @@ class Helper
 
     public static function appendDebuggerBar($request, $response)
     {
+        if ($response->isRedirection() === true) {
+            return $response;
+        }
         $content = $response->getContent();
         $pos = strripos($content, '</body>');
         $barResponse = static::getBar();
