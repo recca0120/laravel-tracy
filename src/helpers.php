50214b3da3e0481bcc50719bb71c7c7bfc1c9c7b
Recca Tsai <recca0120.gmail.com>
2016-07-21 18:28:22 +0800

    fix escapeshellarg disabled

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,26 @@
 <?php
 
-var_dump(function_exists('escapeshellarg'));
+namespace Recca0120\LaravelTracy
+{
+    if (function_exists('escapeshellarg') === true) {
+        function escapeshellarg($input)
+        {
+            return \escapeshellarg($input);
+        }
+    } else {
+        function escapeshellarg($input)
+        {
+            $input = str_replace('\'', '\\\'', $input);
+
+            return '\''.$input.'\'';
+        }
+    }
+}
+
+namespace Tracy
+{
+    function escapeshellarg($input)
+    {
+        return \Recca0120\LaravelTracy\escapeshellarg($input);
+    }
+}
