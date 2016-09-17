93b06f3e2889ab7d404d511b844f1835d824c844
Recca Tsai <recca0120.gmail.com>
2016-09-17 15:01:29 +0800

    fix exception

--- tests/Exceptions/HandlerTest.php
+++ tests/Exceptions/HandlerTest.php
@@ -1,14 +1,13 @@
 <?php
 
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Http\Exception\HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 use Mockery as m;
 use Recca0120\LaravelTracy\BlueScreen;
 use Recca0120\LaravelTracy\Exceptions\Handler;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HandlerTest extends PHPUnit_Framework_TestCase
 {
@@ -17,7 +16,7 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         m::close();
     }
 
-    public function test_handler()
+    public function test_exception_response()
     {
         /*
         |------------------------------------------------------------
@@ -26,14 +25,11 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         */
 
         $exception = new Exception();
-
         $blueScreen = m::mock(BlueScreen::class);
-
         $request = m::mock(Request::class);
-        $responseFactory = m::mock(ResponseFactory::class);
-
         $exceptionHandler = m::mock(ExceptionHandler::class);
-        $handler = new Handler($blueScreen, $responseFactory, $exceptionHandler);
+        $response = m::mock(Response::class);
+        $handler = new Handler($blueScreen, $exceptionHandler);
 
         /*
         |------------------------------------------------------------
@@ -41,9 +37,15 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         |------------------------------------------------------------
         */
 
-        $blueScreen->shouldReceive('render')->with($exception);
-        $responseFactory->shouldReceive('make');
-        $exceptionHandler->shouldReceive('report')->with($exception);
+        $blueScreen->shouldReceive('render')->once()->with($exception);
+
+        $exceptionHandler
+            ->shouldReceive('report')->with($exception)->once()
+            ->shouldReceive('render')->once()->andReturn($response);
+
+        $response
+            ->shouldReceive('getContent')->once()->andReturn($exception)
+            ->shouldReceive('setContent')->once();
 
         /*
         |------------------------------------------------------------
@@ -55,7 +57,7 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         $handler->report($exception);
     }
 
-    public function test_http_exception()
+    public function test_view_response()
     {
         /*
         |------------------------------------------------------------
@@ -63,14 +65,13 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         |------------------------------------------------------------
         */
 
-        $statusCode = 404;
-        $headers = [];
-        $exception = new HttpException($statusCode, null, null, $headers);
+        $exception = new Exception();
         $blueScreen = m::mock(BlueScreen::class);
         $request = m::mock(Request::class);
-        $responseFactory = m::mock(ResponseFactory::class);
-        $exeptionHandler = m::mock(ExceptionHandler::class);
-        $handler = new Handler($blueScreen, $responseFactory, $exeptionHandler);
+        $exceptionHandler = m::mock(ExceptionHandler::class);
+        $response = m::mock(Response::class);
+        $view = m::mock(View::class);
+        $handler = new Handler($blueScreen, $exceptionHandler);
 
         /*
         |------------------------------------------------------------
@@ -78,8 +79,12 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         |------------------------------------------------------------
         */
 
-        $blueScreen->shouldReceive('render')->with($exception)->once();
-        $responseFactory->shouldReceive('make')->with(m::any(), $statusCode, $headers)->once();
+        $exceptionHandler
+            ->shouldReceive('report')->with($exception)->once()
+            ->shouldReceive('render')->once()->andReturn($response);
+
+        $response
+            ->shouldReceive('getContent')->once()->andReturn($view);
 
         /*
         |------------------------------------------------------------
@@ -88,20 +93,42 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         */
 
         $handler->render($request, $exception);
+        $handler->report($exception);
     }
 
-    public function test_http_response_exception()
+    public function test_redirect_response()
     {
-        $exception = new HttpResponseException(m::mock(Response::class));
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
 
+        $exception = new Exception();
         $blueScreen = m::mock(BlueScreen::class);
         $request = m::mock(Request::class);
-        $responseFactory = m::mock(ResponseFactory::class);
+        $exceptionHandler = m::mock(ExceptionHandler::class);
+        $response = m::mock(RedirectResponse::class);
+        $handler = new Handler($blueScreen, $exceptionHandler);
 
-        $exeptionHandler = m::mock(ExceptionHandler::class);
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
 
-        $handler = new Handler($blueScreen, $responseFactory, $exeptionHandler);
+        $exceptionHandler
+            ->shouldReceive('report')->with($exception)->once()
+            ->shouldReceive('render')->once()->andReturn($response);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+
         $handler->render($request, $exception);
+        $handler->report($exception);
     }
 
     public function test_console()
@@ -110,12 +137,11 @@ class HandlerTest extends PHPUnit_Framework_TestCase
         $exception = new Exception();
 
         $blueScreen = m::mock(BlueScreen::class);
-        $responseFactory = m::mock(ResponseFactory::class);
         $exeptionHandler = m::mock(ExceptionHandler::class)
             ->shouldReceive('renderForConsole')->with($output, $exception)->once()
             ->mock();
 
-        $handler = new Handler($blueScreen, $responseFactory, $exeptionHandler);
+        $handler = new Handler($blueScreen, $exeptionHandler);
         $handler->renderForConsole($output, $exception);
     }
 }
