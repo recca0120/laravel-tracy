7f470a468c249659a0d584268102e9bc5d410a8f
Recca Tsai <recca0120.gmail.com>
2017-03-08 10:51:31 +0800

    minify

--- src/Template.php
+++ src/Template.php
@@ -4,6 +4,11 @@ namespace Recca0120\LaravelTracy;
 
 class Template
 {
+    /**
+     * $attributes.
+     *
+     * @var array
+     */
     protected $attributes = [];
 
     /**
@@ -20,7 +25,6 @@ class Template
      * render.
      *
      * @param string $view
-     *
      * @return string
      */
     public function render($view)
@@ -36,54 +40,18 @@ class Template
     /**
      * minify.
      *
-     * @param string $buffer
+     * if need minify style and script, refrence
+     * https://gist.github.com/recca0120/5930842de4e0a43a48b8bf027ab058f9
      *
+     * @param string $buff
      * @return string
      */
-    protected function minify($buffer)
+    protected function minify($buff)
     {
-        $search = [
-            '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
-            '/[^\S ]+\</s',  // strip whitespaces before tags, except space
-            '/(\s)+/s',       // shorten multiple whitespace sequences
-        ];
-
-        $replace = [
-            '>',
-            '<',
-            '\\1',
-        ];
-
-        $buffer = preg_replace($search, $replace, $buffer);
-
-        //replaces
-        $buffer = str_replace(' type="text/javascript"', '', $buffer);
-        $buffer = str_replace(' type="text/css"', '', $buffer);
-
-        //replace style elements
-        $buffer = preg_replace_callback("/<style>([\s\S]*?)<\/style>/", function ($matches) {
-            //minify the css
-            $css = $matches[1];
-            $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
-
-            $css = str_replace(["\r\n", "\r", "\n", "\t", '  ', '    ', '     '], '', $css);
-
-            $css = preg_replace(['(( )+{)', '({( )+)'], '{', $css);
-            $css = preg_replace(['(( )+})', '(}( )+)', '(;( )*})'], '}', $css);
-            $css = preg_replace(['(;( )+)', '(( )+;)'], ';', $css);
-
-            return '<style>'.$css.'</style>';
-        }, $buffer);
-
-        /*
-            //replace script elements
-            $buffer = preg_replace_callback("/<script>([\s\S]*?)<\/script>/", function ($matches) {
-                $minifiedCode = JSMin::minify($matches[1]);
-
-                return '<script>'.$minifiedCode.'</script>';
-            }, $buffer);
-         */
-
-        return $buffer;
+        return preg_replace(
+            ['/<!--(.*)-->/Uis', '/[[:blank:]]+/'],
+            ['', ' '],
+            str_replace(["\n", "\r", "\t"], '', $buff)
+        );
     }
 }
