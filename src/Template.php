7fc24c16d76593231572ea9ff7353dd06822fa55
Recca Tsai <recca0120.gmail.com>
2017-03-08 10:39:42 +0800

    minify

--- src/Template.php
+++ src/Template.php
@@ -20,6 +20,7 @@ class Template
      * render.
      *
      * @param string $view
+     *
      * @return string
      */
     public function render($view)
@@ -35,15 +36,54 @@ class Template
     /**
      * minify.
      *
-     * @param string $html
+     * @param string $buffer
+     *
      * @return string
      */
-    protected function minify($html)
+    protected function minify($buffer)
     {
-        return preg_replace(
-            ['/<!--(.*)-->/Uis', '/[[:blank:]]+/'],
-            ['', ' '],
-            str_replace(["\n", "\r", "\t"], '', $html)
-        );
+        $search = [
+            '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+            '/[^\S ]+\</s',  // strip whitespaces before tags, except space
+            '/(\s)+/s',       // shorten multiple whitespace sequences
+        ];
+
+        $replace = [
+            '>',
+            '<',
+            '\\1',
+        ];
+
+        $buffer = preg_replace($search, $replace, $buffer);
+
+        //replaces
+        $buffer = str_replace(' type="text/javascript"', '', $buffer);
+        $buffer = str_replace(' type="text/css"', '', $buffer);
+
+        //replace style elements
+        $buffer = preg_replace_callback("/<style>([\s\S]*?)<\/style>/", function ($matches) {
+            //minify the css
+            $css = $matches[1];
+            $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
+
+            $css = str_replace(["\r\n", "\r", "\n", "\t", '  ', '    ', '     '], '', $css);
+
+            $css = preg_replace(['(( )+{)', '({( )+)'], '{', $css);
+            $css = preg_replace(['(( )+})', '(}( )+)', '(;( )*})'], '}', $css);
+            $css = preg_replace(['(;( )+)', '(( )+;)'], ';', $css);
+
+            return '<style>'.$css.'</style>';
+        }, $buffer);
+
+        /*
+            //replace script elements
+            $buffer = preg_replace_callback("/<script>([\s\S]*?)<\/script>/", function ($matches) {
+                $minifiedCode = JSMin::minify($matches[1]);
+
+                return '<script>'.$minifiedCode.'</script>';
+            }, $buffer);
+         */
+
+        return $buffer;
     }
 }
