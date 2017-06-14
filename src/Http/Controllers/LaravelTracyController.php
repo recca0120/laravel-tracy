1f5ce71d142cdb7c4afd94a374b4713a688cb72c
recca0120 <recca0120@gmail.com>
2017-06-14 13:11:00 +0800

    add controller

---
+++ src/Http/Controllers/LaravelTracyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Recca0120\LaravelTracy\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Recca0120\LaravelTracy\DebuggerManager;
+use Illuminate\Contracts\Routing\ResponseFactory;
+
+class LaravelTracyController extends Controller
+{
+    /**
+     * index.
+     *
+     * @param \Recca0120\Terminal\Kernel $kernel
+     * @param string $view
+     * @return \Illuminate\Http\Response
+     */
+    public function index(DebuggerManager $debuggerManager, ResponseFactory $responseFactory, $type)
+    {
+        return $responseFactory->stream(function () use ($debuggerManager, $type) {
+            list($headers, $content) = $debuggerManager->dispatchAssets($type);
+            if (headers_sent() === false) {
+                foreach ($headers as $name => $value) {
+                    header(sprintf('%s: %s', $name, $value), true, 200);
+                }
+            }
+            echo $content;
+        }, 200);
+    }
+}
---
+++ tests/Http/Controllers/LaravelTracyControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Recca0120\LaravelTracy\Tests\Http\Controllers;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Recca0120\LaravelTracy\Http\Controllers\LaravelTracyController;
+
+class LaravelTracyControllerTest extends TestCase
+{
+    protected function tearDown()
+    {
+        parent::tearDown();
+        m::close();
+    }
+
+    public function testIndex()
+    {
+        $controller = new LaravelTracyController();
+        $debuggerManager = m::mock('Recca0120\LaravelTracy\DebuggerManager');
+        $responseFactory = m::mock('Illuminate\Contracts\Routing\ResponseFactory');
+
+        $debuggerManager->shouldReceive('dispatchAssets')->once()->andReturn([
+            $headers = ['foo' => 'bar'],
+            $content = 'foo',
+        ]);
+
+        $responseFactory->shouldReceive('stream')->with(m::on(function ($callback) use ($content) {
+            ob_start();
+            $callback();
+            $output = ob_get_clean();
+
+            if (function_exists('xdebug_get_headers') === true) {
+                $this->assertTrue(in_array('foo: bar', xdebug_get_headers(), true));
+            }
+
+            return $content === $output;
+        }), 200)->andReturn(
+            $response = m::mock('Symfony\Component\HttpFoundation\Response')
+        );
+
+        $type = 'foo';
+
+        $this->assertSame($response, $controller->index($debuggerManager, $responseFactory, $type));
+    }
+}
