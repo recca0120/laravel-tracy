02179fa5c8a50b02a2ad382dfe4e29e0edef9b9a
Recca Tsai <recca0120@gmail.com>
2018-08-05 17:17:24 +0800

    tracy-inner-container

--- resources/views/TerminalPanel/panel.php
+++ resources/views/TerminalPanel/panel.php
@@ -1,13 +1,16 @@
 <style class="tracy-debug">
-	#tracy-debug .Laravel-TerminalPanel {background: #000; min-width: 700px; min-height: 500px; padding-bottom:15px;}
+	#tracy-debug .Laravel-TerminalPanel {background: #000; padding-bottom:15px; overflow: hidden}
+    #tracy-debug .tracy-inner-container {min-width: 700px; min-height: 500px;}
 </style>
 
 <h1>Terminal</h1>
 
 <div class="tracy-inner Laravel-TerminalPanel">
-    <?php if (empty($terminal) === false): ?>
-        <?php echo $terminal; ?>
-    <?php else: ?>
-        <span style="color: #fff;">Terminal is Disabled</span>
-    <?php endif ?>
+    <div class="tracy-inner-container">
+        <?php if (empty($terminal) === false): ?>
+            <?php echo $terminal; ?>
+        <?php else: ?>
+            <span style="color: #fff;">Terminal is Disabled</span>
+        <?php endif ?>
+    </div>
 </div>
